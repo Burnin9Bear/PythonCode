3b857e048d27e4f85fc083b15e46cf5f2e84a59f
zzc0130cn <43713175+zzc0130cn@users.noreply.github.com>
2018-12-24 17:33:05 +0800

    20181224

---
+++ 201281224Day3/Section 1 Q1.py
@@ -0,0 +1,22 @@
+# 1. 从键盘中输入5个学生的名字，存储到列表中，然后打印出每个学生名字中的第2个字母
+name_set = []
+name1 = input("input the first student's name")
+name_set.append(name1)
+name2 = input("input the second student's name")
+name_set.append(name2)
+name3 = input("input the third student's name")
+name_set.append(name3)
+name4 = input("input the forth student's name")
+name_set.append(name4)
+name5 = input("input the fifth student's name")
+name_set.append(name5)
+for name_temp in name_set:
+    print(name_temp[1])
+
+
+
+# 3. 老师分配办公室的应用练习
+#    - 输入办公室个数
+#    - 输入老师的个数
+#    - 然后将了老师分配到办公室，保证每个办公室至少一个
+
